e5a719f110f2ab64feaf6b1f6c9adeda4a6bfdec
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2021-12-21 11:30:43 -0300

    Revert "Revert "Commit Inicial""
    This reverts commit 87109574532e0b4e56e23f5562e765f602734c0f.

---
+++ app/config/db.py
@@ -0,0 +1,55 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SQLITE = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
+    }
+}
+
+# psycopg2
+
+POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rrhh',
+        'USER': 'postgres',
+        'PASSWORD': 'ox82',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
+        # 'TIME_ZONE': 'America/Asuncion',
+    }
+}
+
+# mysqlclient
+
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rrhh',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+# pyodbc
+
+SQLSERVER = {
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'rrhh',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'SQL Server Native Client 10.0',
+        },
+    },
+}
